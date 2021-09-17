0e0ca72756dd0422e606c7f13dc3ef4fa31b59ee
Olivier Delaere <23073839+odelaere@users.noreply.github.com>
2021-09-17 15:42:51 +0200

    Fix 4.1.x GHA

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,64 +1,26 @@
 #!/bin/bash
 set -e
 
-COMMANDS="debug help logtail show stop adduser fg kill quit run wait console foreground logreopen reload shell status"
-START="start restart zeoserver"
-CMD="bin/instance1"
-ARGS="$@"
+PRIORIY="instance-cron zeoserver"
+CURL="curl --write-out '%{http_code}' -so /dev/null worker-cron:8087/standard/@@ok"
 
-python2.7 docker-initialize.py
-mkdir -p /data/{log,instance-debug,filestorage,blobstorage,instance-async,instance-amqp,instance1}
+mkdir -p -m 777 /data/{log,instance-debug,filestorage,blobstorage,instance-async,instance-amqp,instance1}
+python docker-initialize.py
 
-if [ -e "custom.cfg" ]; then
-	if [ ! -e "bin/develop" ]; then
-		buildout -c custom.cfg
-		python docker-initialize.py
-	fi
+if [[ "instance" == "$1" || ! $PRIORIY == *"$1"* ]]
+then
+  echo "Wating instance-cron ..."
+  sleep 20
+  response=$($CURL)
+  echo "received response  $response"
+  while [[ "$response" != "'200'" ]]
+  do
+    echo "Waiting instance-cron ..."
+    sleep 10
+    response=$($CURL)
+    echo "received response  $response"
+  done
 fi
+echo "Starting $1"
+exec "bin/python" "bin/$1" "fg"
 
-if [[ "$1" == "zeoserver"* ]]; then
-	CMD="bin/$1"
-fi
-
-if [[ "$1" == "instance-async"* ]]; then
-	CMD="bin/instance-async"
-	ARGS="$CMD $2"
-fi
-
-if [ -z "$HEALTH_CHECK_TIMEOUT" ]; then
-	HEALTH_CHECK_TIMEOUT=1
-fi
-
-if [ -z "$HEALTH_CHECK_INTERVAL" ]; then
-	HEALTH_CHECK_INTERVAL=1
-fi
-
-if [[ $START == *"$1"* ]]; then
-	_stop() {
-		$CMD stop
-		kill -TERM "$child" 2>/dev/null
-	}
-
-	trap _stop SIGTERM SIGINT
-	$CMD start
-    sleep 3
-	$CMD logtail &
-	child=$!
-
-	pid=$($CMD status | sed 's/[^0-9]*//g')
-	if [ -n "$pid" ]; then
-		echo "Application running on pid=$pid"
-		sleep "$HEALTH_CHECK_TIMEOUT"
-		while kill -0 "$pid" 2>/dev/null; do
-			sleep "$HEALTH_CHECK_INTERVAL"
-		done
-	else
-		echo "Application didn't start normally. Shutting down!"
-		_stop
-	fi
-else
-	if [[ $COMMANDS == *"$1"* ]]; then
-		exec $CMD "$ARGS"
-	fi
-	exec $ARGS
-fi
