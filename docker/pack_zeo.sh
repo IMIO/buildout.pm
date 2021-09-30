2bf9f206b3f101f69f938e112c2d46c0a5584406
Olivier Delaere <23073839+odelaere@users.noreply.github.com>
2021-09-30 13:11:24 +0200

    Handle zeo packing

--- docker/pack_zeo.sh
+++ docker/pack_zeo.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 echo "----------Start of Databases packing $(date --rfc-2822)----------"
-
+echo
 CMD="/plone/bin/python /plone/bin/zeopack"
 echo "Executing $CMD"
 eval "$CMD"
 
-if [[ -v "$($MOUNTPOINT)" ]]
+if [ "$MOUNTPOINT" ]
 then
-  echo "Found mountpoint = $($MOUNTPOINT)"
-  CMD="/plone/bin/python /plone/bin/zeopack -S $($MOUNTPOINT) -B /data/blobstorage-$($MOUNTPOINT)"
+  echo "Found mountpoint = $MOUNTPOINT"
+  CMD="/plone/bin/python /plone/bin/zeopack -S $MOUNTPOINT -B /data/blobstorage-$MOUNTPOINT"
   echo "Executing $CMD"
   eval "$CMD"
+else
+  echo "Mountpoint not found"
 fi
-
+echo
 echo "----------End of Databases packing $(date --rfc-2822)----------"
