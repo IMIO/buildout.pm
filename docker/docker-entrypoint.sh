1beab6486a11e954dade2e494018784e0394b83b
Olivier Delaere <23073839+odelaere@users.noreply.github.com>
2021-10-11 12:15:34 +0200

    Improved collective.big.bang managing

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,12 +4,17 @@ set -e
 PRIORIY="instance-cron zeoserver"
 CURL="curl --write-out '%{http_code}' -so /dev/null worker-cron:8087/standard/@@ok"
 
-mkdir -p -m 777 /data/{log,instance-debug,filestorage,blobstorage,instance-async,instance-amqp,instance1}
+mkdir -p -m 777 /data/{log,filestorage,blobstorage}
 python docker-initialize.py
+
+if [[ $MOUNTPOINT ]]
+then
+  mkdir -p -m 777 "/data/blobstorage-$MOUNTPOINT"
+fi
 
 if [[ "instance" == "$1" || ! $PRIORIY == *"$1"* ]]
 then
-  echo "Wating instance-cron ..."
+  echo "Waiting instance-cron ..."
   sleep 20
   response=$($CURL)
   echo "received response  $response"
@@ -21,6 +26,13 @@ then
     echo "received response  $response"
   done
 fi
+
+if [[ "instance-cron" == "$1" ]]
+then
+  echo "Setting ACTIVE_BIGBANG"
+  export ACTIVE_BIGBANG="True"
+fi
+
 echo "Starting $1"
 exec "bin/python" "bin/$1" "fg"
 
