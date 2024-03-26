7e2673ae970bbcd20cdad4a9c206f5cf31d43bd1
Antoine Duchêne <antoine.duchene@imio.be>
2024-03-26 15:16:35 +0100

    Adapt scripts/update_all_local_roles.py

--- scripts/update_all_local_roles.py
+++ scripts/update_all_local_roles.py
@@ -1,9 +1,10 @@
 from plone import api
+from imio.helpers.security import setup_app
 from imio.helpers.security import setup_logger
 import logging
 
-if __name__ == '__main__':
-    setup_logger(level=logging.INFO)
-    with api.env.adopt_user(username="admin"):
-        tool = api.portal.get_tool("portal_plonemeeting")
-        tool.update_all_local_roles(redirect=False, log_batch=1000)
+setup_logger(level=logging.INFO)
+setup_app(app)
+with api.env.adopt_user(username="admin"):
+    tool = api.portal.get_tool("portal_plonemeeting")
+    tool.update_all_local_roles(redirect=False)
